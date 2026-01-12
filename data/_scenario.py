1efa58602713d254cced1b319ecd141977b25a56
MatijsG1 <geertsmatijs@gmail.com>
2026-01-12 15:01:14 +0100

    Scenario masks and names in dict

--- data/_scenario.py
+++ data/_scenario.py
@@ -10,33 +10,33 @@ class Scenario():
     Methods:
         (To be implemented)
     """
-    def __init__(self, dosemap: np.ndarray):
+    def __init__(self, displacement: np.ndarray ,dosemap: np.ndarray, masks: dict[str, np.ndarray]):
+        self.displacement = displacement
         self.dosemap = dosemap
-        self.masks = None
+        self.masks = masks
+     
         self.DVHs = None
-        self.names = None
         
 
     def getAllDVH(self, maxDVH: float = 100.0):
         """
         Compute DVHs for all masks.
         """
-        self.DVHs = []
-        for mask in self.masks:
-            self.DVHs.append(DVH(self.dosemap, mask, maxDVH))
+        self.DVHs = {}
+        for name, mask in self.masks.items():
+            self.DVHs[name] = DVH(self.dosemap, mask, maxDVH)
 
 
     def plot_DVHs(self):
 
             cmap = plt.cm.get_cmap('tab20', max(1, len(self.DVHs)))
             plt.figure()
-            for i in range(len(self.DVHs)):
-                dvh = self.DVHs[i]
-                name = self.names[i]
-                x = np.linspace(0.0, self._maxDVH, len(dvh))
+            for i, (name, dvh_obj) in enumerate(self.DVHs.items()):
+                dvh = dvh_obj.dvh
+                x = np.linspace(0.0, dvh_obj.maxDVH, len(dvh))
                 color = cmap(i if len(self.DVHs) <= 20 else (i % 20))
                 plt.plot(x, dvh, label=name, color=color)
-            plt.xlim(0, self.DVHs[0].maxDVH)
+            plt.xlim(0, list(self.DVHs.values())[0].maxDVH)
             plt.ylim(0, 100)
             plt.xlabel('Dose (Gy)')
             plt.ylabel('Volume (%)')
