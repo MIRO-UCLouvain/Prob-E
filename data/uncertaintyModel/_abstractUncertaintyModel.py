cd2e86ba7b9750a2167070ba40983e22101a82b3
Eliot-P <eliot.peeters@uclouvain.be>
2026-01-12 13:21:57 +0100

    first commit of structure

---
+++ data/uncertaintyModel/_abstractUncertaintyModel.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+
+class AbstractUncertaintyModel(ABC):
+    def __init__(self):
+        """
+        Abstract base class for uncertainty models.
+
+        Attributes
+        ----------
+        name : str
+            The name of the uncertainty model.
+        parameters : dict
+            A dictionary to hold model parameters.
+        """
+        super().__init__()
+        self.name: str = "AbstractUncertaintyModel"
+        self.parameters: dict = {}
+
+    @abstractmethod
+    def pdf(self, x):
+        """
+        Compute the probability density function at point x.
+
+        Parameters
+        ----------
+        x : float
+            The point at which to evaluate the PDF.
+
+        Returns
+        -------
+        float
+            The value of the PDF at point x.
+
+        """
+        pass
+
+    @abstractmethod
+    def cdf(self, x):
+        """
+        Compute the cumulative distribution function at point x.
+
+        Parameters
+        ----------
+        x : float
+            The point at which to evaluate the CDF.
+
+        Returns
+        -------
+        float
+            The value of the CDF at point x.
+
+        """
+        pass
+
+    @abstractmethod
+    def sample(self, n):
+        """
+        Generate n random samples from the uncertainty model.
+
+        Parameters
+        ----------
+        n : int
+            The number of samples to generate.
+
+        Returns
+        -------
+        list
+            A list of n random samples.
+
+        """
+        pass
