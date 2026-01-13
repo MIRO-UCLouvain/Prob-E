1ef404c80b67ae312725f67af1503ee81b8dd77b
Romain Schyns <romain.schyns@uclouvain.be>
2026-01-13 17:34:59 +0100

    created sampling method abstract class

---
+++ Probabilistic_Evaluation/core/sampling.py
@@ -0,0 +1,24 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+class AbstractsamplingMethod:
+    """
+    Abstract base class for sampling methods.
+
+    Attributes
+    ----------
+    name : str
+        The name of the sampling method.
+    parameters : dict
+        A dictionary to hold method parameters.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.name: str = "AbstractsamplingMethod"
+        self.probabilities: np.ndarray = None
+        self.displacements: np.ndarray = None
+
+    @abstractmethod
+    def sample(self):
+        pass
