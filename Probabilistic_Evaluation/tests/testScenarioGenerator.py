2b4dd6a2326bba85ff6d2bddd343951ebbaf4c0e
RoSchyns <romain.schyns@uclouvain.be>
2026-01-14 13:53:56 +0100

    moved tests functions to test folder

---
+++ Probabilistic_Evaluation/tests/testScenarioGenerator.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pytest
+import matplotlib.pyplot as plt
+
+from Probabilistic_Evaluation.core.scenariosGenerator import ScenariosGenerator
+from Probabilistic_Evaluation.data import PatientData
+
+
+class TestScenariosGenerator:
+    def test_scenarios_generator_initialization(self):
+        pass
+
+    def test_probabilistic_scenarios(self):
+        # Example CT and dose images (3D numpy arrays)
+        ctImage = np.zeros((50,50,50))
+        ctImage[15:35,15:35,15:35] = 100  # Example high-density region
+        doseImage = np.zeros((50,50,50))
+        doseImage[20:30,20:30,20:30] = 50  # Example dose distribution
+        targetMask = np.zeros((50,50,50),dtype=bool)  # Example target mask
+        targetMask[22:28,22:28,22:28] = True  # Define target region
+        maskdict = {'Target': targetMask}
+        spacing = (1.0, 1.0, 2.0)  # Example spacing
+
+        patientData = PatientData(ctImage, doseImage, maskdict, spacing=spacing)
+        sigma = 5/3.2
+
+        # test on patient data
+        # assert patient.ctImage.shape == (50,50,50)
+        pytest.approx(patientData.ctImage[20,20,20], 100)
+        pytest.approx(patientData.doseImage[25,25,25], 50)
+        assert patientData.maskDict['Target'][23,23,23] == True
+        
+        ps = ScenariosGenerator(patientData, max_displacement=10,sigmas=(sigma, sigma, sigma), sampling_method='voronoi')
+        
+        assert len(ps.scenarios_list) == ps.voronoi_cells.points.shape[0]
+
+        # print("Evaluation results for all scenarios:", results)
+        #2 plots, one showing the results as afucntion of scenario index and other one show probabilities of voronoi cells as function on cell(scenario) index
+        # plt.subplot(1, 1, 1)
+        # plt.plot(ps.voronoi_cells.probabilities_analytical, label='Analytical')
+        # plt.title("Voronoi Cell Probabilities")
+        # plt.xlabel("Voronoi Cell (Scenario) Index")
+        # plt.ylabel("Probability")
+        # plt.legend()
+        # plt.show()
+
+
+        # for i, point in enumerate(ps.voronoi_cells.points):
+        #     if point.tolist() == [0,0,0]:
+        #         print(f"Scenario {i}: Displacement {point}, Probability: {ps.voronoi_cells.probabilities_analytical[i]}")
+        #     if point.tolist() == [0,1,0]:
+        #         print(f"Scenario {i}: Displacement {point}, Probability: {ps.voronoi_cells.probabilities_analytical[i]}")
+        #     if point.tolist() == [0,0,1]:
+        #         print(f"Scenario {i}: Displacement {point}, Probability: {ps.voronoi_cells.probabilities_analytical[i]}")
+        #         from Probabilistic_Evaluation.utils import shift_dose_image
+        #         shifted_dose = shift_dose_image(ps.doseImage, shift=point)
+        #         plt.imshow(shifted_dose[:,:,25], cmap='jet')
+        #         plt.colorbar(label='Dose')
+        #         plt.contour(ps.targetMask[:,:,25], colors='white', linewidths=0.5)
+        #         plt.title(f"Dose Distribution for Displacement {point}")
+        #         plt.show()
+
+if __name__ == "__main__":
+    TestScenariosGenerator.test_probabilistic_scenarios()
---
+++ Probabilistic_Evaluation/tests/testVoronoiReduction.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pytest
+import matplotlib.pyplot as plt
+
+from Probabilistic_Evaluation.core.voronoiReduction import VoronoiCells
+
+class TestVoronoiReduction:
+    def TestVoronoiCells(limit, sigma=1.6, plot=True):
+        # Generate random points
+        spacing = (1.0, 1.0, 1.0)
+        sigmas = (sigma, sigma, sigma)
+        # Create Voronoi cells
+        voronoi_cells = VoronoiCells(limit, spacing, sigmas, method='both')
+
+        # Monte Carlo simulation to estimate Voronoi cell probabilities
+        probabilitiesMC = voronoi_cells.probabilitiesMC
+        #analytical probabilities
+        probabilities_analytical = voronoi_cells.probabilities_analytical
+        vor_points = voronoi_cells.displacements
+        
+        print("Voronoi Points and Analytical Probabilities:\n")
+        print("vor_ponts.shape:", vor_points.shape)
+        print("probabilities_analytical.shape:", probabilities_analytical.shape)
+        points_probabilities = np.hstack((vor_points, probabilities_analytical.reshape(-1, 1)))
+        # sort by probabilities
+        points_probabilities = points_probabilities[np.argsort(points_probabilities[:, 3])[::-1]]
+        #take the proabibilities needed to reach 95% of cumulative probability
+        cumulative_prob = np.cumsum(points_probabilities[:, 3])
+        num_cells_95 = np.searchsorted(cumulative_prob, 0.95,side='right') + 1
+        threshold_95 = points_probabilities[num_cells_95-1,3]
+        cumulative_prob95 = np.cumsum(points_probabilities[:, 3][points_probabilities[:, 3]>=threshold_95])
+        print(f"Number of Voronoi cells to reach 95% cumulative probability: {len(cumulative_prob95)} and the minimum probability of a scenario to be included: {threshold_95}")
+        num_cells_97 = np.searchsorted(cumulative_prob, 0.97,side='right') + 1
+        threshold_97 = points_probabilities[num_cells_97-1,3]
+        cumulative_prob97 = np.cumsum(points_probabilities[:, 3][points_probabilities[:, 3]>=threshold_97])
+        print(f"Number of Voronoi cells to reach 97% cumulative probability: {len(cumulative_prob97)} and the minimum probability of a scenario to be included: {threshold_97}")
+        num_cells_99 = np.searchsorted(cumulative_prob, 0.99,side='right') + 1
+        threshold_99 = points_probabilities[num_cells_99-1,3]
+        cumulative_prob99 = np.cumsum(points_probabilities[:, 3][points_probabilities[:, 3]>=threshold_99])
+        print(f"Number of Voronoi cells to reach 99% cumulative probability: {len(cumulative_prob99)} and the minimum probability of a scenario to be included: {threshold_99}")
+        num_cells_9999 = np.searchsorted(cumulative_prob, 0.9999,side='right') + 1
+        threshold_9999 = points_probabilities[num_cells_9999-1,3]
+        cumulative_prob9999 = np.cumsum(points_probabilities[:, 3][points_probabilities[:, 3]>=threshold_9999])
+        print(f"Number of Voronoi cells to reach 99.99% cumulative probability: {len(cumulative_prob9999)} and the minimum probability of a scenario to be included: {threshold_9999}")
+
+        assert np.isclose(np.sum(probabilitiesMC), 1.0, atol=1e-2), "Monte Carlo probabilities do not sum to 1."
+        assert np.isclose(np.sum(probabilities_analytical), 1.0, atol=1e-6), "Analytical probabilities do not sum to 1."
+        assert np.allclose(probabilitiesMC, probabilities_analytical, atol=1e-2), "Monte Carlo and Analytical probabilities do not match closely."
+
+        # Plotting
+        if plot:
+            plt.plot(probabilitiesMC,label='Monte Carlo',color='red')
+            plt.plot(probabilities_analytical, label='Analytical',color='blue', linestyle='dashed') #marker = '.')
+            plt.hlines(y=points_probabilities[num_cells_95-1,3], xmin=0, xmax=len(probabilities_analytical), colors='green', linestyles='dotted', label='95% Cumulative Probability Threshold')
+            plt.hlines(y=points_probabilities[num_cells_97-1,3], xmin=0, xmax=len(probabilities_analytical), colors='orange', linestyles='dotted', label='97% Cumulative Probability Threshold')
+            plt.hlines(y=points_probabilities[num_cells_99-1,3], xmin=0, xmax=len(probabilities_analytical), colors='purple', linestyles='dotted', label='99% Cumulative Probability Threshold')
+            plt.hlines(y=points_probabilities[num_cells_9999-1,3], xmin=0, xmax=len(probabilities_analytical), colors='brown', linestyles='dotted', label='99.99% Cumulative Probability Threshold')
+            plt.xlabel('Voronoi Cell Index')
+            plt.ylabel('Probability')
+            plt.title('Voronoi Cell Probabilities: Monte Carlo vs Analytical')
+            plt.legend()
+            plt.show()
+
+if __name__ == "__main__":
+
+    PTVmargin = 5
+    limit = 2*PTVmargin
+    TestVoronoiReduction.TestVoronoiCells(limit,sigma=PTVmargin/(3.2),plot = True)
+
+    # resol = 10
+    # limit = 5
+    # Npixels = resol * limit
+    # Image3Dgaussian = np.zeros((Npixels, Npixels, Npixels))
+    # for i in range(Npixels):
+    #     for j in range(Npixels):
+    #         for k in range(Npixels):
+    #             x = i * limit / Npixels - limit / 2
+    #             y = j * limit / Npixels - limit / 2
+    #             z = k * limit / Npixels - limit / 2
+    #             Image3Dgaussian[i, j, k] = gaussian3D(x, y, z, mu=0, sigma=PTVmargin/(3.2))
+
+    # plt.imshow(Image3Dgaussian[ :, :, int(Npixels/2)])
+    # plt.title('2D Slice of 3D Gaussian Image')
+    # plt.colorbar()
+    # plt.show()
