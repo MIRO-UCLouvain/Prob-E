4c370b0083a2cc23db9f7500f40fdd32da79622d
MatijsG1 <matijs.geerts@uclouvain.be>
2026-09-14 15:08:39 +0200

    change implementation of DMean to numpy implementation

--- src/Probabilistic_Evaluation/data/_DVH.py
+++ src/Probabilistic_Evaluation/data/_DVH.py
@@ -100,8 +100,8 @@ class DVH(object):
     @property
     def DMean(self) -> float:
         if self._DMean is None:
-            
-            self._DMean = np.sum(self.dosemap[self.mask>0]*self.mask[self.mask>0]) / np.sum(self.mask[self.mask>0])
+        
+            self._DMean = np.average(self.dosemap[self.mask>0], weights=self.mask[self.mask>0])
             
         return self._DMean
 
